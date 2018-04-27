e7f567834932ea590436c2dfdd3c54a57655413a
Will <rev3ks@gmail.com>
2018-04-26 18:29:38 -0600

    added files broken off from trading_algorithm

---
+++ prompts.py
@@ -0,0 +1,67 @@
+def prompt_user():
+  '''Prompts User for input to start algorithm, returns trading_terms. 
+  '''
+  ready = True
+  print(" ***********************Trading Algorithm************************ \
+    \n ***********************By William P. Fey************************ \
+    \n\n" +\
+    "This program will list a squence of buy and sell trades on \n"+ \
+    "GDAX.com based on the follosing input.\n")
+  while ready:
+    try:
+      # Sizes of Trades
+      print("Available Trading Pairs\n"+\
+        "BTC-USD, ETH-USD, LTC-USD, BCH-USD, BTC-ETH, LTC-BTC, "+\
+        "BCH-BTC\n")
+      pair = input("What trading pair would you like to list?\n\n")
+      budget = float(input((
+        "\nWhat is the value of {0} would you like to allocate in "+\
+        "terms of {1}?\n\n").format(pair[:3],pair[4:])))
+      print("\nSize of trades")
+      min_size = float(input("\nWhat is the minimum trade size for this "+\
+         "pair?\n\n"))
+      size_change = float(input("\nHow much should each trade in the "+ \
+        "sequnce of buys and sells \nincrease by?\n\n"))
+      
+      print("\nPrices of trades\n")
+      current_price = float(\
+        input(\
+        ("What is the estimated price of {0} in terms of {1}?\n\n")\
+        .format(pair[:3], pair[4:])))
+      use_current_price = (input(\
+        ("\nWould you like to use {0} {1}/{2} as the the midpoint of "+\
+         "the \ntrading algorithm? (y or n)\n\n")\
+        .format(current_price, pair[4:], pair[:3])))
+      if 'y' != use_current_price[:1].lower():
+        mid_price = float(input("\nWhat midpoint price would you "+\
+          "like to use?\n\n"))
+      else: mid_price = current_price
+      
+      high_price = float(input(\
+        "\nWhat is the highest price to be sold at?\n\n"))
+      low_price = 2* mid_price - high_price
+      
+    # if input raises errors, ask if user would like to try again
+    except:
+      retry = input("Input was incorrectly formatted would you like "+\
+        "to retry? (y or n)")     
+      retry = retry[:1].lower()
+      ready = 'y' == retry
+      continue
+    
+    trading_terms = TradingTerms(pair, budget, low_price, mid_price, min_size, size_change)
+
+    return trading_terms
+
+  # if user exits inuput returns -1 to disrupt main from running. 
+  return -1
+
+def prompt_ready_to_trade():
+  if 'y' == input("Would you like trades to be listed? (y or n)\n")[:1].lower():
+    return True
+  elif 'n' == input("Would you like to change input? (y or n)\n")[:1].lower():
+    return False 
+
+def prompt_to_return_class():
+  if 'y' == input("Would you like to return trades as a class? (y or n)\n")[:1].lower():
+    return True
---
+++ trading.py
@@ -0,0 +1,91 @@
+def send_trade_list(
+  pair, 
+  side, 
+  first_trade_price, 
+  first_trade_size, 
+  price_increase, 
+  size_increase, 
+  trade_count, 
+  auth):
+  """function takes in intial info trading pair (BTC-USD), side (buy or 
+  sell), first trade price, minimum trade value, increase in price per trade,
+  increase in value per trade, the number of trades and an authorization
+  token and lists a corrosponding sequence of trades on GDAX through there
+  API
+  
+  To do's : write function to accept class object instead of each item
+  """
+  
+  # Initiate trading index, trade dictionary to be sent in while-loop, and
+  # neg_pos variable to help manage buy vs sell sequence direction in loop. 
+  n = 0
+  ts = []
+  trade = {
+    "size": "",
+    "price": "",
+    "side": side,
+    "product_id": pair
+  }
+  if side == "buy":
+    neg_pos = -1
+  else:
+    neg_pos = 1
+  api_url = 'https://api.gdax.com/'
+  
+  # While loop to list each trade in sequence
+  while n < trade_count:
+    trade["size"] = str(
+      round(first_trade_size + size_increase * n, 10) 
+      )
+    trade["price"] = str(
+      round(
+        first_trade_price + neg_pos * price_increase * n, 10
+        ) 
+      )
+    t = requests.post(api_url + 'orders', json=trade, auth=auth)
+    
+    if t.status_code != 200:
+      print(("Response: {0}, Message {1}, Price: {2}, Size: {3}").format(
+        str(t.status_code),
+        t.json()["message"],
+        trade["price"],
+        trade["size"],
+        )
+      )
+
+    else: 
+      try:
+        print(("{0}, {1}, Size: {2}, Price: {3}").format(
+          t.json()["product_id"],
+          t.json()["side"],
+          t.json()["size"],
+          t.json()["price"],
+          )
+        )
+        ts.append( t.json() ) 
+      
+      except:
+        print(t.json)
+    n += 1
+
+  return ts
+
+def auth_and_list_trades(pair, side, first_trade_size, size_increase, first_trade_price, price_change, trade_count ):
+  '''
+  creates auth token and lists sequence of trades on Exchange given a trading pair, side, first_trade_size, size_increase, first_trade_price, price_change, trade_count
+  '''
+  # set up authorization token and factors needed to send trades.
+  auth = autho.run_GdaxAuth()
+  
+  # Send trades.
+  ts = send_trade_list(
+    pair,
+    side, 
+    first_trade_price, 
+    first_trade_size, 
+    price_change, 
+    size_increase, 
+    trade_count, 
+    auth)
+  
+  return ts
