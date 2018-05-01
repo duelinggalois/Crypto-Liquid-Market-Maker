bbb1ffb731661c21e5fad4ddc7aa4753794531b4
Will <william.p.fey@gmail.com>
2018-05-01 01:23:47 -0600

    Reorganized Socket started customizing message

--- subscribe.py
+++ subscribe.py
@@ -25,72 +25,77 @@ class Subscribe():
     self.ws = None
     self.file_path=file_path
     self.stop= False
-    
-  async def start(self):
-    self.sub_params = create_sub_params()
-    async with websockets.connect( self.url ) as self.ws:
-      await self.ws.send( json.dumps( sub_params ))
-      await listen()
 
-    asyncio.get_event_loop().run_until_complete(self.ws) 
-  
-  def create_sub_params(self):     
-    
     if self.channel == []:
-      sub_params = {
+      self.sub_params = {
         'type': subscription, 
         'product_ids': self.product_ids}
     else:
-      sub_params = {
+      self.sub_params = {
         'type': subscription, 
         'product_ids': self.product_ids, 
-        'channels': self.channel 
-      }
+        'channels': self.channel
+        }
+
+  def auth_stamp(self):     
+    self.api_key = config.api_key
+    self.api_secret = config.api_secret
+    self.api_passphrase= config.api_passphrase
+    timestamp = str(time.time())
+    message = timestamp + 'GET' + '/users/self/verify'
+    message = message.encode()
+    hmac_key = base64.b64decode(self.api_secret)
+    signature = hmac.new(hmac_key, message, hashlib.sha256)
+    signature_b64 = base64.b64encode(signature.digest()).decode()
+    self.sub_params['signature'] = signature_b64
+    self.sub_params['key'] = self.api_key
+    self.sub_params['passphrase'] = self.api_passphrase
+    self.sub_params['timestamp'] = timestamp
+
+  def start(self):
+
+    if auth:
+      auth_stamp()
+    self.ws = self._connect()
+
+    asyncio.get_event_loop().run_until_complete(self.ws)  
+
+
+  async def _connect(self):
+    async with websockets.connect(self.url) as ws:
+      await ws.send( json.dumps( self.sub_params ))
+      await self._listen(ws)
+
+    return ws
     
-    if self.auth:
-      self.api_key = config.api_key
-      self.api_secret = config.api_secret
-      self.api_passphrase= config.api_passphrase
-      timestamp = str(time.time())
-      message = timestamp + 'GET' + '/users/self/verify'
-      message = message.encode()
-      hmac_key = base64.b64decode(self.api_secret)
-      signature = hmac.new(hmac_key, message, hashlib.sha256)
-      signature_b64 = base64.b64encode(signature.digest()).decode()
-      sub_params['signature'] = signature_b64
-      sub_params['key'] = self.api_key
-      sub_params['passphrase'] = self.api_passphrase
-      sub_params['timestamp'] = timestamp
-    
-    return sub_params
-  
-  async def listen(self):
+  async def _listen(self, ws):
     subsc = False
     while True:
       try:
-        msg = await asyncio.wait_for(self.ws.recv(), timeout=20)
+        msg = await asyncio.wait_for(ws.recv(), timeout=20)
         while not subsc:
-          # check for "type":"subscriptions"
-          if msg.json()["type"] == "subsciptions":
-            on_open(msg) 
-          else:
-            self.on_message(msg)
+          # need to check for "type":"subscriptions"
+          # self.on_open(msg)
+          self.on_message(msg)
+          subsc = True
+
+        self.on_message(msg)
       # ping socket to keep connection alive
       except asyncio.TimeoutError:
+        print("\n--pinging socket--\n")
         try:
-          pong_socket = await websocket.ping()
+          pong_socket = await ws.ping()
           await asyncio.wait_for(pong_socket, timeout=10)
-          print("\n--pinging socket--\n")
         except:
           print("\n--no pong from socket--\n")
           break
       
       except Exception as e:
         self.on_error(e)
-        self._disconnect(e)
       
 
-  def _disconnect(self):
+  def _disconnect(self, e):
+    print(e)
     self.on_close()
   
   def close(self):
@@ -98,9 +103,8 @@ class Subscribe():
   
   def on_open(self, msg):
     print("\n-- Subscribed! --\n")
-    print("Pairs: {0}\nChannels: {1}".format(
-      msg.json()["product_ids"],
-      msg.json()["channels"]
+    print("{0}".format(
+      msg
       )
     )
   
