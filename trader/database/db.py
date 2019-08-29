ac02f8b65012dcfdeed706146bd333ea368ea3cd
Will <rev3ks@gmail.com>
2019-08-28 20:56:13 -0600

    Adjusted thread pool refresh and adjusted error handling
    Once the cause of #56 was determined, I was able to do
    two things to address the issue. First I was able to clean
    up the error handling so that when disconnects occur, they
    are handled properly without having to restart the trader
    socket manager. Second, I added pool recycle time to the
    database engine that would refresh the pool before the 60
    second mysql thread timeout would invalidate each connection.
    Now this specific disconnect is eliminated, and future
    unknown disconnects are handled gracefully. Also excessive
    logging errors for canceled orders not found or already done
    messages changed to warnings and code refactored to create
    less of those instances. Closes #56

--- trader/database/db.py
+++ trader/database/db.py
@@ -13,18 +13,25 @@ class DataAccessLayer:
   def __init__(self):
       self.engine = None
       self.conn_string = None
+      self.Session_Factory = None
+      self.Session = None
 
   def connect(self, test=True):
     if not self.conn_string:
       self.set_connection_string(test)
-    self.engine = create_engine(self.conn_string, echo_pool=True,
-                                pool_pre_ping=True)
+    self.engine = create_engine(
+      self.conn_string,
+      pool_size=config.POOL_RECYCLE,
+      max_overflow=config.MAX_OVERFLOW,
+      pool_recycle=config.POOL_RECYCLE
+    )
     BaseWrapper.metadata.create_all(self.engine)
     self.Session_Factory = sessionmaker(bind=self.engine)
     self.Session = scoped_session(self.Session_Factory)
 
   def set_connection_string(self, test=True):
     self.conn_string = _db_connection_string(test)
+
 
 dal = DataAccessLayer()
 
